d94e01a11179227cc30f595a3e6089e819a31824
acn3to <costanetoow@gmail.com>
2024-01-31 00:01:32 -0300

    test: check streamlit page title and h1

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ import subprocess
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture
@@ -21,3 +22,23 @@ def driver():
 def test_app_opens(driver):
     driver.get("http://localhost:8501")
     sleep(5)
+
+
+def test_check_title_is(driver):
+    driver.get("http://localhost:8501")
+    sleep(5)
+
+    page_title = driver.title
+
+    expected_title = "Excel Schema Validator"
+    assert page_title == expected_title, f"The page title was '{page_title}', but expected '{expected_title}'"
+
+
+def test_check_streamlit_h1(driver):
+    driver.get("http://localhost:8501")
+    sleep(5)
+
+    h1_element = driver.find_element(By.TAG_NAME, "h1")
+
+    expected_text = "Insert your spreadsheet for validation"
+    assert h1_element.text == expected_text
